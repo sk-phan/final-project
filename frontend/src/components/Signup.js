88f419ff1d0d1a81973cfbad3968a86357639e83
kolkri <kristiinakolu@gmail.com>
2022-06-06 17:23:15 +0300

    brach push

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch, batch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { user } from "../reducers/user";
-import stylyed from 'styled-components'
+import styled from 'styled-components'
 
 //SIGNUP
 export const Signup = () => {
@@ -105,154 +105,264 @@ export const Signup = () => {
   console.log(password, username, 'test')
 
   return (
-       <div>
-         <h2> Create an account </h2>
-         <p> Make pet experience better </p>
-
-         <form>
-           <label htmlFor='Pet sitter'>
-            <input 
-              id='Pet sitter'
-              type = 'radio' 
-              value = 'Pet sitter' 
-              checked = {profileType === 'Pet sitter'}
-              onChange = {(e) => setProfileType(e.target.value)}
-            />
-            Pet sitter
-           </label>
-           <label htmlFor='Pet owner'>
-            <input 
-              id='Pet owner'
-              type = 'radio' 
-              value = 'Pet owner' 
-              checked = {profileType === 'Pet owner'}
-              onChange = {(e) => setProfileType(e.target.value)}
-            />
-            Pet owner
-           </label>
-           <input 
-             type='text'
-             value={username}
-             onChange = {(e) => setUsername(e.target.value)}
-           />
-           <label htmlFor='email'>
-             Email
-            <input 
-              id='email'
-              type='email'
-              value={email}
-              onChange = {(e) => setEmail(e.target.value)}
-            />
-           </label>
-           <label htmlFor='password'>
-              Password
-              <input 
-                id='password'
-                type='password'
-                value={password}
-                onChange = {(e) => setPassword(e.target.value)}
+    <Main>
+       <FormContainer>
+         <FormTitle> Create an account </FormTitle>
+         <FormSubTitle> Make pet experience better </FormSubTitle>
+         <Form>
+             <div className="radio-container">
+              <RadioLabel htmlFor='Pet sitter'>
+                <RadioInput
+                  id='Pet sitter'
+                  type = 'radio' 
+                  value = 'Pet sitter' 
+                  checked = {profileType === 'Pet sitter'}
+                  onChange = {(e) => setProfileType(e.target.value)}
+                />
+                Pet sitter
+              </RadioLabel>
+              <RadioLabel htmlFor='Pet owner'>
+              <RadioInput
+                id='Pet owner'
+                type = 'radio' 
+                value = 'Pet owner' 
+                checked = {profileType === 'Pet owner'}
+                onChange = {(e) => setProfileType(e.target.value)}
               />
-           </label>
-           <label htmlFor='rePassword'>
-              Re-enter password
-              <input 
+              Pet owner
+            </RadioLabel>
+           </div >
+          <div className="input-container">
+              <input
+                id="username"
+                className="input"
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+              />
+              <label className="user-label" htmlFor="username">
+                Username
+              </label>
+            </div>
+            <div className="input-container">
+              <input
+                id="email"
+                className="input"
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+              <label className="user-label" htmlFor="email">
+                Email
+              </label>
+            </div>
+            <div className="input-container">
+              <input
+                id="password"
+                className="input"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+              <label className="user-label" htmlFor="password">
+                Password
+              </label>
+            </div>
+            <div className="input-container">
+              <input
                 id='rePassword'
-                type='password'
+                className="input"
+                type="rePassword"
                 value={rePassword}
-                onChange = {(e) => setRePassword(e.target.value)}
-            />
-           </label>
-           <label html='img'>
+                onChange={(e) => setRePassword(e.target.value)}
+                required
+              />
+              <label className="user-label" htmlFor="rePassword">
+                Re-enter password
+              </label>
+            </div>
+            
+            <div className="input-container">
+           
              <input type='file'
                      name="myImage"
                      onChange={(e) => upload(e)}
             />
-           </label>
-           <span>Pet Information</span>
-           <label htmlFor='dog'>
-             <input 
-                id='dog'
-                type='radio'
-                value='dog'
-                checked = {animalType === 'dog'}
-                onChange= {(e) => setAnimalType(e.target.value)}
-            />
-            Dog
-           </label>
-           <label htmlFor='cat'>
-             <input 
+             <label htmlFor='img' className="user-label">
+               Profile image
+            </label>
+          </div>
+          <div className="radio-container">
+              <RadioLabel htmlFor='dog'>
+                <RadioInput
+                  id='dog'
+                  type = 'radio' 
+                  value = 'dog' 
+                  checked = {animalType === 'dog'}
+                  onChange = {(e) => setAnimalType(e.target.value)}
+                />
+                Dog
+              </RadioLabel>
+              <RadioLabel htmlFor='cat'>
+              <RadioInput
                 id='cat'
-                type='radio'
-                value='cat'
+                type = 'radio' 
+                value = 'cat' 
                 checked = {animalType === 'cat'}
-                onChange= {(e) => setAnimalType(e.target.value)}
-            />
-            Cat
-           </label>
+                onChange = {(e) => setAnimalType(e.target.value)}
+              />
+              Cat
+            </RadioLabel>
+           </div >
+           
+           
+           <div className="checkbox-container">
            {preferTimeOption.map(item => {
-             return <label htmlFor={item}>
-                    <input 
+             return <RadioLabel htmlFor={item}>
+                    <RadioInput
                       type='checkbox'
                       value = {item}
                       checked = {preferableTime.includes(item)}
                       onChange = { () => onTimeCheckbox(item) }      
                     />
                     {item}
-             </label>
+             </RadioLabel>
            })}
-           <label htmlFor='start-date'>
-              Start date
-              <input 
-                type = 'date'
-                id = 'start-date'
+           </div>
+
+           <div className="input-container">
+              <input
+                id="start-date"
+                className="input"
+                type="date"
                 value={startDate}
-                onChange = {(e) => setStartDate(e.target.value)}
+                onChange={(e) => setStartDate(e.target.value)}
+                required
               />
-           </label>
-           <label htmlFor='end-date'>
-              End date
-              <input 
-                type = 'date'
-                id = 'end-date'
+              <label className="user-label" htmlFor="start-date">
+                Start Date
+              </label>
+            </div>
+           
+            <div className="input-container">
+              <input
+                id="end-date"
+                className="input"
+                type="date"
                 value={endDate}
-                onChange = {(e) => setEndDate(e.target.value)}
+                onChange={(e) => setEndDate(e.target.value)}
+                required
               />
-           </label>
-           <button type='submit' onClick={onFormSubmit}>Sign up</button>
-         </form>
-       </div>
+              <label className="user-label" htmlFor="end-date">
+                End Date
+              </label>
+            </div>
 
+          
+           <SignupButton type='submit' onClick={onFormSubmit}>Sign up</SignupButton>
+         </Form>
+         <P>Have an account? <LinkText>Log in</LinkText></P>
+       </FormContainer>
 
+    </Main>
 
-
-    // <div className="App">
-    //   <form onSubmit={onFormSubmit}>
-
-    //      <label>Username
-    //      <input
-    //       type='text'
-    //       value={username}
-    //       onChange={(e) => setUsername(e.target.value)}
-    //       />
-    //       </label>
-
-    //       <label>Password
-    //      <input
-    //       type='password'
-    //       value={password}
-    //       onChange={(e) => setPassword(e.target.value)}
-    //       />
-    //       </label>
-
-
-    //          <input
-    //           type="file"
-    //           name="myImage"
-    //           onChange={(e) => upload(e)}
-    //     />
-    //   <img src={img}  width="200px" />
-    //       <button type='submit'>Sign up</button>
-    //   </form>
-    // </div>
   );
 }
+
+const Main = styled.main`
+    width: 100%;
+    min-width: 100vw;
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: url("https://images.unsplash.com/photo-1551730459-92db2a308d6a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80") no-repeat center center fixed; 
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+`
+
+const FormContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 2rem;    
+    width: 35rem;
+    border-box: box-sizing;
+    padding: 2rem;
+    margin:2rem;
+    background:rgba(255,255,255, 0.9);
+    position:relative;
+`
+const Form = styled.form`
+    display:flex;
+    flex-direction: column;
+    
+`
+
+const FormTitle = styled.h1`
+    font-family: 'Raleway', sans-serif;
+    font-style: normal;
+    font-weight: 700;
+    font-size: 24px;
+    line-height: 28px;
+    letter-spacing: 0.035em;
+    text-align:center;
+`
+
+const FormSubTitle = styled.p`
+    font-family: 'Raleway', sans-serif;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 19px;
+    text-align: center;
+    color: #666666;
+    margin: 0 0 2rem 0;
+    padding:0;
+`
+
+
+const SignupButton = styled.button`
+ 
+    border: none;
+    background-color: #FD9951;
+    border-radius: 1rem;
+    cursor: pointer;
+    color: #fff;
+    padding: 1.5rem;
+    margin: 12px;
+    font-weight: 600;
+    font-size: 1.6rem;
+`
+
+const P = styled.p`
+    font-family: 'Raleway', sans-serif;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 1.6rem;
+`
+
+const LinkText = styled.span`
+    font-family: 'Raleway', sans-serif;
+    font-size: 1.6rem;
+    color: #FD9951;
+    font-weight: 600;
+`
+
+
+const RadioLabel = styled.label`
+  display:flex;
+  align-items: center;
+  font-size: 14px;
+`
+
+const RadioInput = styled.input`
+  width: fit-content;
+  color:#FD9951;
+`
