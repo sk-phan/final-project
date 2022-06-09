84befb4c903e26f2983fdcd1a78709080c9f22de
kolkri <kristiinakolu@gmail.com>
2022-06-09 12:33:27 +0300

    add dasboard styling

--- frontend/src/pages/Userpage.js
+++ frontend/src/pages/Userpage.js
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { useNavigate, Link } from "react-router-dom";
 import { user } from "../reducers/user";
 import styled from 'styled-components'
+import moment from 'moment';
 
 export const Userpage = () => {
 
@@ -60,13 +61,13 @@ export const Userpage = () => {
               <Img src={user.img} />
             </ProfileImageContainer>
             <ProfileTextContainer>
-              <ProfileTitle>{user.username} - {user.profileType}</ProfileTitle>
-              {user.profileType === 'Pet sitter' ? <ProfileText>Looking for {user.animalType}s to take care of</ProfileText> : <ProfileText>Looking for a pet sitter for a {user.animalType}</ProfileText>}
+              <ProfileTitle>@{user.username}</ProfileTitle>
+              {user.profileType === 'Pet sitter' ? <ProfileText>{user.profileType} for {user.animalType}s </ProfileText> : <ProfileText>Looking for a {user.animalType} pet sitter</ProfileText>}
               <Tags>
                 {user.preferableTime.map(time => {
                 return <Tag>{time}</Tag>})}
               </Tags>
-              <ProfileText>Dates: {user.startDate} - {user.endDate}</ProfileText>
+              <ProfileText>{moment(user.startDate).format('MMM Do YY')} - {moment(user.endDate).format('MMM Do YY')}</ProfileText>
 
             </ProfileTextContainer>
             <Overlay></Overlay>
@@ -134,8 +135,9 @@ const UserContainer = styled(Link)`
 
 const ProfileImageContainer = styled.div`
     width: 100%;
-    height: 40%;
+    height: 50%;
     background-color: red;
+    box-sizing: border-box;
 
 `
 
@@ -143,15 +145,22 @@ const Img = styled.img`
   width: 100%;
   height: 100%;
   object-fit: cover;
+  box-sizing: border-box;
 `
 const ProfileTextContainer = styled.div`
     width: 100%;
-    height: 60%;
+    height: 50%;
     padding:5px;
     gap: 5px;
     display: flex;
     flex-direction: column;
+    justify-content: space-evenly;
     box-sizing: border-box;
+
+    @media (min-width: 1025px) {
+      padding:10px;
+      gap: 10px;
+     }
     
 `
 const ProfileTitle = styled.h1`
@@ -159,6 +168,10 @@ const ProfileTitle = styled.h1`
   font-weight: 700;
   font-size: 12px;
   margin:0;
+  color: #000;
+  @media (min-width: 768px) {
+    font-size: 18px;
+   }
 `
 
 const ProfileText = styled.p`
@@ -167,19 +180,32 @@ const ProfileText = styled.p`
   font-weight: 500;
   font-size: 10px;
   margin:0;
+  @media (min-width: 768px) {
+    font-size: 14px;
+   }
 `
 const Tags = styled.div`
     display: flex;
     flex-wrap: wrap;
     gap: 5px;
+    
 `
 const Tag = styled.span`
   
-  background-color: #FF974B;
-  padding: 2px;
-  color: #fff;
+  background-color: #e5f2eb;
+  padding: 3px;
+  color: #000;
   width: fit-content;
-  font-weight: 700;
+  font-weight: 500;
+  border-radius:5px;
+  font-size: 8px;
+  text-transform: uppercase;
+  box-shadow: 4px 4px 5px rgba(0, 0, 0, 0.04);
+  @media (min-width: 768px) {
+    font-size: 10px;
+    border-radius:7px;
+    padding: 5px;
+   }
 `
 
 const Overlay = styled.div`
